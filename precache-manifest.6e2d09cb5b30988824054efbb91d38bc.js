b0bcfc4d46f7c69690b7ee0e5db58a7f51113b12
reeveng <govaertreeven@gmail.com>
2020-09-13 16:30:07 +0200

    Updates

---
+++ precache-manifest.6e2d09cb5b30988824054efbb91d38bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa08d753b9adc339b14990ca8a50b0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16f16402b141dae4797c",
+    "url": "/static/css/main.217054a3.chunk.css"
+  },
+  {
+    "revision": "c00a98d6473a252512e2",
+    "url": "/static/js/2.628bcca5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.628bcca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f16402b141dae4797c",
+    "url": "/static/js/main.7b511445.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "018838cb2f4612f99779db287d765bbd",
+    "url": "/static/media/BigJohnPRO-Bold.018838cb.otf"
+  },
+  {
+    "revision": "90e40e07fbf00054a602da9974edfc22",
+    "url": "/static/media/BigJohnPRO-Light.90e40e07.otf"
+  },
+  {
+    "revision": "9b550f15f8938c843c72650d7bad6034",
+    "url": "/static/media/BigJohnPRO-Regular.9b550f15.otf"
+  },
+  {
+    "revision": "252c335fca76766d34bc4e7a2e3312d5",
+    "url": "/static/media/logo.252c335f.svg"
+  },
+  {
+    "revision": "1ae05f6b7269d0df38e5e1dda69b602f",
+    "url": "/static/media/working.1ae05f6b.svg"
+  }
+]);
