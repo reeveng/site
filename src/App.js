c81381faa5395bf478c193c0f0e685d5116faab1
reeveng <govaertreeven@gmail.com>
2020-11-15 10:26:00 +0100

    remove comments and center social section about

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import './styles/styles.scss';
 import { Route, Switch } from 'react-router-dom';
 import Loading from './components/Loading';
 
-// pages
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Project from './pages/Project';
 
-// function getEmail() {
-//   console.log("%cYou're welcome ;) govaertreeven@gmail.com")
-// }
-
 function App() {
-  // console.log("%cHey, do you have questions? run `getEmail()`", "")
   return (
     <div className="app">
       <Switch>
