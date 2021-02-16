961ccdc679d78250bf6a8b854d773c23d8ab8835
reeveng <govaertreeven@gmail.com>
2021-02-16 22:52:53 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,72 @@
 import React, { useEffect } from 'react';
 import { ReactComponent as Working } from "../assets/vector/working.svg"
 import { ReactComponent as ArrowRight } from "../assets/vector/arrowRight.svg"
+import { ReactComponent as ArrowLeft } from "../assets/vector/arrowLeft.svg"
 import ProjectCard from '../components/ProjectCard';
 import { projects } from "../assets/json/projects";
 import { useWindowSize } from '../components/OnResize';
 
 let moveSpeed = 0;
 let firstTime = true;
+let nr = 0;
 
 const onScrollHomepage = () => {
   let homepageDiv = document.getElementById("homepage");
   homepageDiv.addEventListener("wheel", (event) => {
-    if (firstTime) {
-      moveSpeed = Math.floor(window.innerWidth / 100)
-      moveSpeed = moveSpeed <= 10 ? 10 : moveSpeed;
-      if (window.innerWidth > 900) {
-        if (moveSpeed <= 37) {
-          moveSpeed = 37;
+    if (nr > 1) {
+      console.log();
+      if (firstTime) {
+        moveSpeed = Math.floor(window.innerWidth / 100);
+        if (window.innerWidth > 900) {
+          if (moveSpeed <= 37) {
+            moveSpeed = 37;
+          }
+        } else {
+          moveSpeed = moveSpeed <= 10 ? 10 : moveSpeed;
         }
+        firstTime = false;
+        return;
       }
-      document.getElementById("arrowRight").classList.add("visible");
-      firstTime = false;
-      return;
+      nr = 0;
+
+      if (event.deltaY < 0) {
+        homepageDiv.scrollLeft -= moveSpeed;
+        return;
+      }
+      if (event.deltaY > 0) {
+        homepageDiv.scrollLeft += moveSpeed;
+        return;
+      }
     }
-    if (event.deltaY < 0) {
-      homepageDiv.scrollLeft -= moveSpeed;
-      return;
-    }
-    if (event.deltaY > 0) {
-      homepageDiv.scrollLeft += moveSpeed;
-      return;
-    }
-  })
+    nr++;
+    arrows();
+    return;
+  });
 }
 
+let arrows = () => {
+  let home = Math.abs(document.getElementById("home").getBoundingClientRect().x);
+  let about = Math.abs(document.getElementById("about").getBoundingClientRect().x);
+  let projects = Math.abs(document.getElementById("projects").getBoundingClientRect().x);
+  let closest = Math.min(home, about, projects);
+  if (closest === home) {
+    document.getElementById("arrowLeft").classList.add("invisible");
+    document.getElementById("arrowRight").classList.remove("invisible");
+    return;
+  }
+  if (closest === about) {
+    document.getElementById("arrowRight").classList.remove("invisible");
+    document.getElementById("arrowLeft").classList.remove("invisible");
+    return;
+  }
+  if (closest === projects) {
+    document.getElementById("arrowRight").classList.add("invisible");
+    document.getElementById("arrowLeft").classList.remove("invisible");
+    return;
+  }
+}
+
+window.addEventListener('load', arrows);
 
 const Home = () => {
   const [width,] = useWindowSize();
@@ -45,23 +78,99 @@ const Home = () => {
     onScrollHomepage()
   }, []);
 
-
   return (
     <>
-      {/* <Nav smallNav /> */}
+      <div className="arrowLeft"
+        style={
+          {
+            position: "fixed",
+            left: 0,
+            bottom: 0,
+            margin: "1rem",
+            padding: "1rem"
+          }
+        }>
+        <ArrowLeft id="arrowLeft"
+          style={
+            {
+              width: 30,
+              height: 30
+            }
+          } onClick={
+            (e) => {
+              let home = Math.abs(document.getElementById("home").getBoundingClientRect().x);
+              let about = Math.abs(document.getElementById("about").getBoundingClientRect().x);
+              let projects = Math.abs(document.getElementById("projects").getBoundingClientRect().x);
+              let closest = Math.min(home, about, projects);
 
+              if (closest === about) {
+                document.getElementById('home').scrollIntoView();
+                arrows();
+                return;
+              }
+              if (closest === projects) {
+                document.getElementById('about').scrollIntoView();
+                arrows();
+                return;
+              }
+              if (closest === home) {
+                return;
+              }
+            }} />
+      </div>
+      <div className="arrowRight"
+        style={
+          {
+            position: "fixed",
+            right: 0,
+            bottom: 0,
+            margin: "1rem",
+            padding: "1rem"
+          }
+        }>
+        <ArrowRight id="arrowRight"
+          style={
+            {
+              width: 30,
+              alignSelf: "flex-end",
+              height: 30,
+              justifySelf: "flex-end"
+            }
+          } onClick={
+            (e) => {
+              let home = Math.abs(document.getElementById("home").getBoundingClientRect().x);
+              let about = Math.abs(document.getElementById("about").getBoundingClientRect().x);
+              let projects = Math.abs(document.getElementById("projects").getBoundingClientRect().x);
+              let closest = Math.min(home, about, projects);
+
+              if (closest === home) {
+                document.getElementById('about').scrollIntoView();
+                arrows();
+                return;
+              }
+              if (closest === about) {
+                document.getElementById('projects').scrollIntoView();
+                arrows();
+                return;
+              }
+              if (closest === projects) {
+                return;
+              }
+            }} />
+      </div>
       <div id="homepage">
-
-        <div className="pane" id="home">
+        <div className="pane"
+          id="home">
           <div className="content">
             <h1 className="title">
               Reeven
             </h1>
-            <div className="content-body" style={{ display: "flex", flexDirection: "column" }}>
+            <div className="content-body"
+              style={{ display: "flex", flexDirection: "column" }}>
               <div id="working-svg">
-                <Working id="working" style={{ maxWidth: width - 20 }} />
+                <Working id="working"
+                  style={{ maxWidth: width - 20 }} />
               </div>
-              <ArrowRight id="arrowRight" style={{ width: 30, height: 30, marginTop: "5%" }} onClick={() => { document.getElementById('about').scrollIntoView(); }} />
             </div>
           </div>
         </div>
