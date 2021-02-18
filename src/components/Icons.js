a37d7a7a9e1955a6a5655af215eea5fb41935cf1
reeveng <govaertreeven@gmail.com>
2021-02-18 12:36:02 +0100

    update added githubtimeline to projects

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,11 +1,13 @@
 
 import Github from "../assets/vector/socialMedia/github.svg"
+import GithubPages from "../assets/vector/socialMedia/githubPages.svg"
 import Gmail from "../assets/vector/socialMedia/gmail.svg"
 import Dribbble from "../assets/vector/socialMedia/dribbble.svg"
 import LinkedIn from "../assets/vector/socialMedia/linkedin.svg"
 
 export const socialMedia = {
   github: Github,
+  githubpages: GithubPages,
   gmail: Gmail,
   mail: Gmail,
   dribbble: Dribbble,
